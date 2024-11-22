2b65035ba484919891a46b308d24871f6d84ebb6
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-11-21 20:12:26 -0600

    Initial Commit

---
+++ Flickr Photo App/Flickr_Photo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_Photo_AppApp.swift
+//  Flickr Photo App
+//
+//  Created by Dmytro Ryshchuk on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_Photo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
